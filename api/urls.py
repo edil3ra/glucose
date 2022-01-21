cc8a67f52e8beadf689618f6ee0c72c8c8360d71
edil3ra <edil3ra@gmail.com>
2022-01-21 18:21:06 +0100

    added filter on timestamp

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListGlucose, DetailGlucose, DetailGlucoseByUser
+from .views import ListGlucose, DetailGlucose, ListGlucoseByUser, DetailGlucoseByUser
 
 urlpatterns = [
-    path('v1/glucoses/user/<int:pk>', DetailGlucoseByUser.as_view()),
-    path('v1/glucoses/<int:pk>/', DetailGlucose.as_view()),
-    path('v1/glucoses/', ListGlucose.as_view()),
+    path('v1/levels/<int:pk>/', DetailGlucose.as_view()),
+    path('v1/levels/', ListGlucose.as_view()),
+    path('v1/levels/user/<int:pk>/', ListGlucoseByUser.as_view()),
+    path('v1/levels/<int:pk>/user/<int:pk_user>/', DetailGlucoseByUser.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,29 @@
 from rest_framework import generics
 from .models import Glucose
 from .serializers import GlucoseSerializer
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+import django_filters
+
+
+
+class TimestampFilter(django_filters.FilterSet):
+    start = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr='gte')
+    end = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr='lte')
+
+    class Meta:
+        model = Glucose
+        fields = ['start', 'end']
 
 
 class ListGlucose(generics.ListCreateAPIView):
     queryset = Glucose.objects.all()
     serializer_class = GlucoseSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_class = TimestampFilter
+
+    # filterset_fields = ['timestamp__range']
+
 
 
 class DetailGlucose(generics.RetrieveUpdateDestroyAPIView):
@@ -13,11 +31,23 @@ class DetailGlucose(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = GlucoseSerializer
 
 
-# TODO
-class DetailGlucoseByUser(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Glucose.objects.all()
+class ListGlucoseByUser(generics.ListCreateAPIView):
     serializer_class = GlucoseSerializer
-
+    filter_class = TimestampFilter
     def get_queryset(self):
-        User.objects.filter(Q(invited_by=request.user.id)|Q(id = request.user.id))
+        user_id = self.kwargs['pk']
+        User = get_user_model()
+        user = get_object_or_404(User, pk=user_id)
+        return Glucose.objects.filter(user=user)
+
+
+class DetailGlucoseByUser(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = GlucoseSerializer
+    def get_queryset(self):
+        glucose_id = self.kwargs['pk']
+        user_id = self.kwargs['pk_user']
+        User = get_user_model()
+        get_object_or_404(User, pk=user_id)
+        get_object_or_404(Glucose, pk=glucose_id)
+        return Glucose.objects.filter(user__pk=user_id, pk=glucose_id)
 
